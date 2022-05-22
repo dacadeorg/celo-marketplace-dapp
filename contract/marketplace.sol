2c240e2fb85820b7edaee2ac37bb40de0d8a9509
Jaylukmann <lafbnyunus@yahoo.com>
2022-05-22 13:11:30 +0100

    Added 10% commision for the smart contract owner and restricted product owner self-buy

--- contract/marketplace.sol
+++ contract/marketplace.sol
@@ -15,7 +15,7 @@ interface IERC20Token {
 }
 
 contract Marketplace {
-
+    address internal contractOwner;
     uint internal productsLength = 0;
     address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
@@ -30,6 +30,11 @@ contract Marketplace {
     }
 
     mapping (uint => Product) internal products;
+
+     	//CONSTRUCTOR
+	constructor () payable  {
+		contractOwner = msg.sender;
+	}
 
     function writeProduct(
         string memory _name,
@@ -72,13 +77,27 @@ contract Marketplace {
     }
     
     function buyProduct(uint _index) public payable  {
-        require(
-          IERC20Token(cUsdTokenAddress).transferFrom(
+        //Adjusting price for commission
+         uint adjustedPrice = (products[_index].price * 90)/100;
+         uint commissionPrice =(products[_index].price * 10)/100;
+
+        //Restricting product owner(seller) from buying his/her product
+        require(msg.sender != products[_index].owner,"You cannot buy your own product");
+
+        //Sending 10% of the product price to the contract owner
+        require(IERC20Token(cUsdTokenAddress).transferFrom(
+             msg.sender,
+             contractOwner,
+             commissionPrice
+        ), "Transfer to contract owner failed");
+
+        //Sending the remaining 90% to the product owner(seller)
+        require(IERC20Token(cUsdTokenAddress).transferFrom(
             msg.sender,
             products[_index].owner,
-            products[_index].price
+            adjustedPrice
           ),
-          "Transfer failed."
+          "Transfer to artwork owner failed."
         );
         products[_index].sold++;
     }
--- src/main.js
+++ src/main.js
@@ -169,7 +169,7 @@ document
     } catch (error) {
       notification(`⚠️ ${error}.`)
     }
-    notification(`🎉 You successfully added "${params[0]}".`)
+    notification(`🎉 You successfully added "${params[0]}" at 10% discount.When bought,you will receive 90% of its price.`)
     getProducts()
   })
 
